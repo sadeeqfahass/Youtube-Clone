5dfe1f121410f255214f511695f600e55cecfee4
sadeeq fahass <sadeeqfhassan@gmail.com>
2023-05-22 21:13:48 +0100

    #

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,6 +3,7 @@ import VideoCard from "./VideoCard";
 
 function Videos() {
   const [videoData, setVideoData] = useState([]);
+
   useEffect(() => {
     async function getYouTubeVideos() {
       const url =
@@ -19,20 +20,8 @@ function Videos() {
       try {
         const response = await fetch(url, options);
         const data = await response.json();
-        setVideoData(data.items)
-        console.log(data.items)
-        // const videosEl = data.items.map((item) => {
-        //   const { id, snippet, contentDetails, statistics } = item;
-        //   return (
-        //     <VideoCard
-        //       id={id}
-        //       title={snippet.title}
-        //       duration={contentDetails.duration}
-        //       viewCount={statistics.viewCount}
-        //       thumbnail={snippet.thumbnails.default.url}
-        //     />
-        //   );
-        // });
+        setVideoData(data.items);
+        console.log(data.items);
       } catch (error) {
         console.error(error);
       }
@@ -41,12 +30,22 @@ function Videos() {
     getYouTubeVideos();
   }, []);
 
-  
-
-
-  return <div className="videos-container">
-            {/* {videosEl} */}
-        </div>;
+  return (
+    <div className="videos-container">
+      {/* {videoData.map((item) => {
+        const { id, snippet, contentDetails, statistics } = item;
+        return (
+          <VideoCard
+            id={id}
+            title={snippet.title}
+            duration={contentDetails.duration}
+            viewCount={statistics.viewCount}
+            thumbnail={snippet.thumbnails.default.url}
+          />
+        );
+      })} */}
+    </div>
+  );
 }
 
 export default Videos;
